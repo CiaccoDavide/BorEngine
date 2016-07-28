7d741009f55559832f311469e6e6c7e6346cda0b
Ciacco Davide <ciaccodavide@gmail.com>
2016-07-28 20:59:10 +0200

    reupload prep

--- BorEngine/ResourcesManager.cpp
+++
@@ -1,9 +0,0 @@
-#include "ResourcesManager.h"
-
-
-GLTexture ResourcesManager::getTexture(std::string texturePath)
-{
-	return _textureCache.getTexture(texturePath);
-}
-GLuint  ResourcesManager::boundTexture;
-TextureCache ResourcesManager::_textureCache;
